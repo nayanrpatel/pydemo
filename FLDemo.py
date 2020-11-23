15e90a18b55dec41e62b48080d45893be276ce56
Nayan Patel <53246326+nayanrpatel@users.noreply.github.com>
2020-11-23 13:34:51 -0500

    Add files via upload

---
+++ FLDemo.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+import json
+
+def getName():
+    mname = {}
+    mname["first-name"]="Nayan"
+    mname["last-name"]="patel"
+    return mname
+
+if __name__ == "__main__":
+    print(getName())
